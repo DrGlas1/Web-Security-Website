0efa5ce8f6ab891fd240783166b9617b1be5be3c
Joar Rinaldo-Roos <jo6788ri-s@student.lu.se>
2023-11-22 11:27:28 +0100

    added salt to hash, and refactored out dbconfig

--- login.php
+++ login.php
@@ -1,10 +1,14 @@
-<?php
-session_start();
 
-	//Enter own values for $dbHost etc.
-		$conn = pg_connect("host=$dbHost port=$dbPort dbname=$dbName user=$dbUser password=$dbPassword");
+<?php
+//Make a config file for you database
+include('dbconfig.php');
+session_start();
+//Protects against Cross-Site Request Forgery (CSRF)
+	$csrfToken = bin2hex(random_bytes(32));
+	$_SESSION['csrf_token'] = $csrfToken;
+	$conn = pg_connect("host={$dbConfig['host']} port={$dbConfig['port']} dbname={$dbConfig['dbname']} user={$dbConfig['user']} password={$dbConfig['password']}");
 	$authenticated = false;
-	
+
 	
 function handleLogin($conn, $user, $pwd) {
 	$query = 'SELECT id, password FROM users WHERE username = $1';
@@ -14,7 +18,8 @@ function handleLogin($conn, $user, $pwd) {
 	if ($result) {
 			$authenticated = password_verify($pwd, $result->password);
 			if ($authenticated) {
-					$_SESSION['id'] = $result->id; // Adjust the field names as needed
+					session_regenerate_id();
+					$_SESSION['id'] = $result->id; 
 					echo 'Login successful!';
 			}
 			else {
@@ -28,10 +33,11 @@ function handleLogin($conn, $user, $pwd) {
 
 function handleRegistration($conn, $reg_user, $reg_pwd) {
 	// Hash the password using Bcrypt
-	$hashed_password = password_hash($reg_pwd, PASSWORD_BCRYPT);
+	$salt = bin2hex(random_bytes(22)); // 22 bytes for a 32-character salt
+	$hashed_password = password_hash($reg_pwd . $salt, PASSWORD_BCRYPT);
 	// Insert the user into the database with the hashed password
-	$query = 'INSERT INTO users (username, password) VALUES ($1, $2)';
-	pg_query_params($conn, $query, array($reg_user, $hashed_password));
+	$query = 'INSERT INTO users (username, password, salt) VALUES ($1, $2, $3)';
+	pg_query_params($conn, $query, array($reg_user, $hashed_password, $salt)); //You need to pass this into ALTER TABLE users ADD COLUMN salt VARCHAR(44);
 }
 	if(isset($_POST['verify']) && $_POST['verify'] =='Verify') {
 		if($conn) {
@@ -64,6 +70,7 @@ function handleRegistration($conn, $reg_user, $reg_pwd) {
 			handleRegistration($conn, $reg_user, $reg_pwd);
 		}
 	}
+	
 ?>
 
 <!DOCTYPE html>
@@ -74,11 +81,13 @@ function handleRegistration($conn, $reg_user, $reg_pwd) {
 		<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
 			Username: <input type="text" name="user" /><br/>
 			Password: <input type="password" name="pwd" /><br/>
+			<input type="hidden" name="csrf_token" value="<?php echo bin2hex(random_bytes(32)); ?>" />
 			<input type="submit" value="Verify" name="verify" />
 		</form>
 		<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
 			Username: <input type="text" name="reg_user" /><br/>
 			Password: <input type="password" name="reg_pwd" /><br/>
+			<input type="hidden" name="csrf_token" value="<?php echo bin2hex(random_bytes(32)); ?>" />
 			<input type="submit" value="Register" name="register" />
 		</form>
 	</body>
