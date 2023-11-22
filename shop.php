c8bdd149011feba369b62ca8dec0f8768caffa4a
Luc Sommerland <lu1522so-s@student.lu.se>
2023-11-22 16:23:24 +0100

    Button event for adding to cart

--- shop.php
+++ shop.php
@@ -8,25 +8,34 @@
 <h1>Welcome to the shop!</h1>
 
 <?php
-  include 'inventory.php';
+    include 'inventory.php';
 
-  echo '<table border="1">';
-  //echo '<tr><th>Item</th><th>Price</th><th>Image</th></tr>';
+    echo '<table border="1">';
+    //echo '<tr><th>Item</th><th>Price</th><th>Image</th></tr>';
 
-  foreach ($inventory as $item => $details) {
-      echo '<tr>';
-      echo '<td>' . $item . '</td>';
-      echo '<td>' . $details['price'] . ' kr</td>';
-      echo '<td><img src="' . $details['image_url'] . '" alt="' . $item . '" height="100"></td>';
-      echo '<td><form method="post" action="cart.php">
-                <input type="hidden" name="item" value="' . $item . '">
-                <input type="submit" name="add_to_cart" value="Add to Cart">
-              </form></td>';
-      echo '</tr>';
-  }
+    foreach ($inventory as $item => $details) {
+        echo '<tr>';
+        echo '<td>' . $item . '</td>';
+        echo '<td>' . $details['price'] . ' kr</td>';
+        echo '<td><img src="' . $details['image_url'] . '" alt="' . $item . '" height="100"></td>';
+        echo '<td><button onclick="increaseQuantity(\'' . $item . '\')">Add to cart</button></td>';
+    }
 
-  echo '</table>';
-  echo '<br><a href="cart.php"><button>Go to Cart</button></a>';
+    if(isset($_POST['add_to_cart'])) {
+        // Call the update_cart function and pass the item name
+        if(isset($_POST['item'])) {
+            $itemName = $_POST['item'];
+            update_cart($itemName, 7);
+        }
+    }
+
+    echo '</table>';
+    echo '<br><a href="cart.php"><button>Go to Cart</button></a>';
 ?>
+<script>
+    function increaseQuantity(item) {
+        console.log(item);
+    }
+</script>
 </body>
 </html>
