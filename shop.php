aaf3f7c2b3fac7e3651e12c4941fe18f41fb5d9d
Luc Sommerland <lu1522so-s@student.lu.se>
2023-11-27 12:34:51 +0100

    code cleanup

--- shop.php
+++ shop.php
@@ -5,19 +5,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $cart = $_SESSION["cart"];
 
-    // Update the quantity for the specified item
     if (array_key_exists($itemName, $cart)) {
         $cart[$itemName]['quantity']++;
-        // Return the updated quantity as a response
         echo $cart[$itemName]['quantity'];
     }
 
     $_SESSION["cart"] = $cart;
-    // You can store the updated cart in a session variable or database for future use
-    // For now, the cart will revert to its original state upon page reload
 }
 ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -47,17 +42,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 <script>
     function increaseQuantity(itemName) {
-        // Send an AJAX request to update the quantity in PHP
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.status === 200) {
-                    // Quantity updated successfully, update the UI if needed
-                    // For instance, you might update the displayed quantity
-                    // in the user interface
                     //updateUI(itemName, xhr.responseText);
                 } else {
-                    // Handle error response
                     console.error('Error:', xhr.status);
                 }
             }
