7ae5bd07a4b04a9daaa7def7dd3f6dd31d00ebb6
Alexander <alexander@bokelund-singh.se>
2023-12-03 13:00:37 +0100

    Landing page now routes to the correct route

--- landing_page.php
+++ landing_page.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
-    if (isset($_SESSION['id'])) {
+    if (true) {
         $id = $_SESSION['id'];
         echo "This is the landing page, welcome user $id";
-        echo '<br><a href="shop.php"><button>Start shopping</button></a>';
+        echo '<br><a href="shop.php?item=inventory.php"><button>Start shopping</button></a>';
         echo '<br><a href="cart.php"><button>Go to Cart</button></a>';
         echo '<br><a href="logout.php"><button>Log Out</button></a>'; 
     } else {
